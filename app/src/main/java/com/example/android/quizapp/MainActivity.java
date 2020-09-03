b288837ed2bbc6ea4fe193d5c21e47550ac2b8b0
Carlos Wilson <cawilson@OneIncSystems.com>
2020-09-02 20:11:14 -0500

    Adding more changes

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -4,14 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button backButton;
+    Button nextButton;
+    LinearLayout page1;
+    LinearLayout page2;
+    LinearLayout page3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        backButton = findViewById(R.id.back_button);
+        nextButton = findViewById(R.id.next_button);
+
+        page1 = (LinearLayout) findViewById(R.id.page_1);
+        page2 = (LinearLayout) findViewById(R.id.page_2);
+        page3 = (LinearLayout) findViewById(R.id.page_3);
+
+        page2.setVisibility(View.GONE);
+        page3.setVisibility(View.GONE);
+
+        backButton.setVisibility(View.GONE);
     }
 
     public void onRadioButtonClicked(View view) {
@@ -20,11 +41,11 @@ public class MainActivity extends AppCompatActivity {
 
         // Check which radio button was clicked
         switch(view.getId()) {
-            case R.id.answer_4:
+            case R.id.answer_1:
                 if (checked)
                     // Pirates are the best
                     break;
-            case R.id.answer_5:
+            case R.id.answer_2:
                 if (checked)
                     // Ninjas rule
                     break;
@@ -36,5 +57,46 @@ public class MainActivity extends AppCompatActivity {
      */
     public void resetScores(View view) {
 
+    }
+
+    /**
+     * Resets and displays score for both teams
+     */
+    public void goNext(View view) {
+        Button nextButton = (Button) view;
+        String buttonText = nextButton.getText().toString();
+        Toast.makeText(this, buttonText, Toast.LENGTH_SHORT).show();
+
+        if (buttonText.equals("Finish")) {
+            showScore();
+        } else {
+            page1.setVisibility(View.GONE);
+            page2.setVisibility(View.VISIBLE);
+
+            backButton.setVisibility(View.VISIBLE);
+            nextButton.setText("Finish");
+        }
+    }
+
+    /**
+     * Resets and displays score for both teams
+     */
+    public void goBack(View view) {
+        page1.setVisibility(View.VISIBLE);
+        page2.setVisibility(View.GONE);
+        nextButton.setText("Next");
+        backButton.setVisibility(View.GONE);
+        nextButton.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Resets and displays score for both teams
+     */
+    public void showScore() {
+        page1.setVisibility(View.GONE);
+        page2.setVisibility(View.GONE);
+        page3.setVisibility(View.VISIBLE);
+        backButton.setVisibility(View.GONE);
+        nextButton.setVisibility(View.GONE);
     }
 }
